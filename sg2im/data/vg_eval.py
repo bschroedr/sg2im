4d8e3c2160d7e7d8052b12a52e90e3266db74848
Brigit Schroeder <brigit@Brigits-MacBook-Pro.local>
2021-01-14 21:58:36 -0800

    evaluation version of VG with some extra metadata

---
+++ sg2im/data/vg_eval.py
@@ -0,0 +1,247 @@
+#!/usr/bin/python
+#
+# Copyright 2018 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import random
+from collections import defaultdict
+
+import torch
+from torch.utils.data import Dataset
+import torchvision.transforms as T
+
+import numpy as np
+import h5py
+import PIL
+
+from .utils import imagenet_preprocess, Resize
+
+
+class VgSceneGraphDataset(Dataset):
+  def __init__(self, vocab, h5_path, image_dir, image_size=(256, 256),
+               normalize_images=False, max_objects=10, max_samples=None,
+               include_relationships=True, use_orphaned_objects=True, seed=0):
+    super(VgSceneGraphDataset, self).__init__()
+
+    # set random seed 
+    self.seed = seed 
+    #if self.seed != 0:
+    #  print('graph randomization turned off')
+    #  random.seed(self.seed)
+
+    self.image_dir = image_dir
+    self.image_size = image_size
+    self.vocab = vocab
+    self.num_objects = len(vocab['object_idx_to_name'])
+    self.num_preds = len(vocab['pred_name_to_idx'])
+    self.use_orphaned_objects = use_orphaned_objects
+    self.max_objects = max_objects
+    self.max_samples = max_samples
+    self.include_relationships = include_relationships
+
+    transform = [Resize(image_size), T.ToTensor()]
+    if normalize_images:
+      transform.append(imagenet_preprocess())
+    self.transform = T.Compose(transform)
+
+    self.data = {}
+    with h5py.File(h5_path, 'r') as f:
+      for k, v in f.items():
+        if k == 'image_paths':
+          self.image_paths = list(v)
+        elif k == 'image_urls':
+          self.image_urls = list(v)
+        elif k == 'image_data':
+          self.image_data = list(v)
+        else:
+          self.data[k] = torch.IntTensor(np.asarray(v))
+
+  def __len__(self):
+    num = self.data['object_names'].size(0)
+    if self.max_samples is not None:
+      return min(self.max_samples, num)
+    return num
+
+  def __getitem__(self, index):
+    """
+    Returns a tuple of:
+    - image: FloatTensor of shape (C, H, W)
+    - objs: LongTensor of shape (O,)
+    - boxes: FloatTensor of shape (O, 4) giving boxes for objects in
+      (x0, y0, x1, y1) format, in a [0, 1] coordinate system.
+    - triples: LongTensor of shape (T, 3) where triples[t] = [i, p, j]
+      means that (objs[i], p, objs[j]) is a triple.
+    """
+    if self.seed != 0:
+      random.seed(self.seed)
+      self.seed += 1
+
+    # Image web URL
+    url = self.image_urls[index]
+    image = []
+    if self.image_dir is not None: 
+      img_path = os.path.join(self.image_dir, self.image_paths[index])
+      with open(img_path, 'rb') as f:
+        with PIL.Image.open(f) as image:
+          WW, HH = image.size
+          image = self.transform(image.convert('RGB'))
+    else:
+      # Set original image height and width using image metadata
+      WW = self.data['width'][index]
+      HH = self.data['height'][index]
+      #print(WW,HH,self.image_data[index])
+
+    H, W = self.image_size
+    # Figure out which objects appear in relationships and which don't
+    obj_idxs_with_rels = set()
+    obj_idxs_without_rels = set(range(self.data['objects_per_image'][index].item()))
+    for r_idx in range(self.data['relationships_per_image'][index]):
+      s = self.data['relationship_subjects'][index, r_idx].item()
+      o = self.data['relationship_objects'][index, r_idx].item()
+      obj_idxs_with_rels.add(s)
+      obj_idxs_with_rels.add(o)
+      obj_idxs_without_rels.discard(s)
+      obj_idxs_without_rels.discard(o)
+
+    obj_idxs = list(obj_idxs_with_rels)
+    obj_idxs_without_rels = list(obj_idxs_without_rels)
+    if len(obj_idxs) > self.max_objects - 1:
+      obj_idxs = random.sample(obj_idxs, self.max_objects)
+    if len(obj_idxs) < self.max_objects - 1 and self.use_orphaned_objects:
+      num_to_add = self.max_objects - 1 - len(obj_idxs)
+      num_to_add = min(num_to_add, len(obj_idxs_without_rels))
+      obj_idxs += random.sample(obj_idxs_without_rels, num_to_add)
+    O = len(obj_idxs) + 1
+
+    objs = torch.LongTensor(O).fill_(-1)
+    attrs = torch.LongTensor(O, self.data['object_attributes'][index, 0].size(0) ).fill_(-1) 
+    num_attrs = torch.LongTensor(O).fill_(-1) 
+
+    boxes = torch.FloatTensor([[0, 0, 1, 1]]).repeat(O, 1)
+    obj_idx_mapping = {}
+    for i, obj_idx in enumerate(obj_idxs):
+      objs[i] = self.data['object_names'][index, obj_idx].item() 
+      # add attributes per object
+      attrs[i] = self.data['object_attributes'][index, obj_idx] 
+      num_attrs[i] = self.data['attributes_per_object'][index, obj_idx].item()
+      x, y, w, h = self.data['object_boxes'][index, obj_idx].tolist()
+      x0 = float(x) / WW
+      y0 = float(y) / HH
+      x1 = float(x + w) / WW
+      y1 = float(y + h) / HH
+      boxes[i] = torch.FloatTensor([x0, y0, x1, y1])
+      obj_idx_mapping[obj_idx] = i
+
+    # The last object will be the special __image__ object
+    objs[O - 1] = self.vocab['object_name_to_idx']['__image__']
+
+    triples = []
+    for r_idx in range(self.data['relationships_per_image'][index].item()):
+      if not self.include_relationships:
+        break
+      r_id = self.data['relationship_ids'][index, r_idx].item()
+      s = self.data['relationship_subjects'][index, r_idx].item()
+      p = self.data['relationship_predicates'][index, r_idx].item()
+      o = self.data['relationship_objects'][index, r_idx].item()
+      s = obj_idx_mapping.get(s, None)
+      o = obj_idx_mapping.get(o, None)
+      if s is not None and o is not None:
+        triples.append([s, p, o, r_id])
+
+    # Add dummy __in_image__ relationships for all objects
+    in_image = self.vocab['pred_name_to_idx']['__in_image__']
+    for i in range(O - 1):
+      #triples.append([i, in_image, O - 1])
+      triples.append([i, in_image, O - 1, -1]) # relationship id = -1
+
+    triples = torch.LongTensor(triples)
+    return image, objs, boxes, triples, num_attrs, attrs, url
+
+
+def vg_collate_fn(batch):
+  """
+  Collate function to be used when wrapping a VgSceneGraphDataset in a
+  DataLoader. Returns a tuple of the following:
+
+  - imgs: FloatTensor of shape (N, C, H, W)
+  - objs: LongTensor of shape (O,) giving categories for all objects
+  - boxes: FloatTensor of shape (O, 4) giving boxes for all objects
+  - triples: FloatTensor of shape (T, 3) giving all triples, where
+    triples[t] = [i, p, j] means that [objs[i], p, objs[j]] is a triple
+  - obj_to_img: LongTensor of shape (O,) mapping objects to images;
+    obj_to_img[i] = n means that objs[i] belongs to imgs[n]
+  - triple_to_img: LongTensor of shape (T,) mapping triples to images;
+    triple_to_img[t] = n means that triples[t] belongs to imgs[n].
+    num_attrs: LongTensor of shape (O,) giving number of attributes for all objects
+    attrs: LongTensor of shape (O,max_num_attrs) giving lists of attributes for all objects
+  """
+  # batch is a list, and each element is (image, objs, boxes, triples, num_attrs, attributes, url) 
+  all_imgs, all_objs, all_boxes, all_triples, all_num_attrs, all_attrs, all_urls = [], [], [], [], [], [], []
+  all_obj_to_img, all_triple_to_img = [], []
+  obj_offset = 0
+  for i, (img, objs, boxes, triples, num_attrs, attrs, url) in enumerate(batch):
+    all_imgs.append(img[None])
+    O, T = objs.size(0), triples.size(0)
+    all_objs.append(objs)
+    all_boxes.append(boxes)
+    triples = triples.clone()
+    triples[:, 0] += obj_offset
+    triples[:, 2] += obj_offset
+    all_triples.append(triples)
+    all_num_attrs.append(num_attrs)
+    all_attrs.append(attrs)
+    all_urls.append(url)
+
+    all_obj_to_img.append(torch.LongTensor(O).fill_(i))
+    all_triple_to_img.append(torch.LongTensor(T).fill_(i))
+    obj_offset += O
+
+  all_imgs = torch.cat(all_imgs)
+  all_objs = torch.cat(all_objs)
+  all_boxes = torch.cat(all_boxes)
+  all_triples = torch.cat(all_triples)
+  all_num_attrs = torch.cat(all_num_attrs)
+  all_attrs = torch.cat(all_attrs)
+  all_obj_to_img = torch.cat(all_obj_to_img)
+  all_triple_to_img = torch.cat(all_triple_to_img)
+  all_num_attrs = torch.cat(all_num_attrs)
+  all_attrs = torch.cat(all_attrs)
+  all_urls = torch.cat(all_urls)
+  
+  out = (all_imgs, all_objs, all_boxes, all_triples,
+         all_obj_to_img, all_triple_to_img, all_num_attrs, all_attrs, all_urls)
+  return out
+
+
+def vg_uncollate_fn(batch):
+  """
+  Inverse operation to the above.
+  """
+  imgs, objs, boxes, triples, obj_to_img, triple_to_img = batch
+  out = []
+  obj_offset = 0
+  for i in range(imgs.size(0)):
+    cur_img = imgs[i]
+    o_idxs = (obj_to_img == i).nonzero().view(-1)
+    t_idxs = (triple_to_img == i).nonzero().view(-1)
+    cur_objs = objs[o_idxs]
+    cur_boxes = boxes[o_idxs]
+    cur_triples = triples[t_idxs].clone()
+    cur_triples[:, 0] -= obj_offset
+    cur_triples[:, 2] -= obj_offset
+    obj_offset += cur_objs.size(0)
+    out.append((cur_img, cur_objs, cur_boxes, cur_triples))
+  return out
+
